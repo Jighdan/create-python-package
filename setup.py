aa4b71106e58b4179eabd9aa5af00880574ee189
Reinny <reynsth@gmail.com>
2020-07-02 14:49:50 -0400

    structure rework

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(
-	name = "create_py_pkg",
+	name = "create_python_pkg",
 	author = "Reinny Almonte",
 	author_email = "reynsth@gmail.com",
 	url = "https://github.com/jighdan/create_python_package",
+        description = "A simple python package template generator",
 	version = "0.1.0",
 	entry_points = {
 		"console_scripts": [
-			"create_py_pkg = create_py_pkg.__main__:main"
+			"create_python_pkg = create_python_pkg.__main__:main"
 		]
-	}
+	},
+        python_requires=">=3.6"
 )
